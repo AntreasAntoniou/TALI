4b1507d8572ce42b86a4c49199d64945109e8af6
Antreas Antoniou <iam@antreas.io>
2023-05-10 20:27:26 +0100

    Add more upload scripts

---
+++ tali_wit/scripts/upload_git_lfs.py
@@ -0,0 +1,75 @@
+from pathlib import Path
+import subprocess
+from tqdm import tqdm
+import fire
+
+
+def upload_lfs_files(target_dir):
+    # Convert to a Path object
+    target_dir = Path(target_dir)
+
+    # Git LFS needs to be initialized per repository
+    subprocess.run(["git", "lfs", "install"], cwd=target_dir)
+
+    # Find all files in the directory and its subdirectories
+    files = list(target_dir.glob("**/*"))
+
+    # Filter out directories, hidden files and files inside hidden directories
+    files = [f for f in files if f.is_file() and not ".git" in f.parts]
+
+    total = len(files)
+
+    # Initialize the progress bar
+    pbar = tqdm(total=total, ncols=70)
+
+    for count, file in enumerate(files, 1):
+        # Track the file with Git LFS
+        subprocess.run(["git", "lfs", "track", str(file)], cwd=target_dir)
+
+        # Add the file to the repository
+        subprocess.run(["git", "add", str(file)], cwd=target_dir)
+
+        # If we've processed 10 files, commit and push
+        if count % 10 == 0:
+            subprocess.run(
+                [
+                    "git",
+                    "commit",
+                    "-m",
+                    f"Adding files {count - 9} to {count}",
+                ],
+                cwd=target_dir,
+            )
+            subprocess.run(["git", "push", "origin", "main"], cwd=target_dir)
+
+            # Delete the last 10 files from the local system
+            for i in range(count - 9, count + 1):
+                files[i].unlink()
+
+            # Update the progress bar
+            pbar.update(10)
+
+    # If there are any remaining files (less than 10), commit and push those
+    if count % 10 != 0:
+        subprocess.run(
+            ["git", "commit", "-m", "Adding remaining files"], cwd=target_dir
+        )
+        subprocess.run(["git", "push", "origin", "main"], cwd=target_dir)
+
+        # Delete the remaining files from the local system
+        for i in range(count - count % 10 + 1, count + 1):
+            files[i].unlink()
+
+        # Final update to the progress bar
+        pbar.update(count % 10)
+
+    pbar.close()
+
+
+def main():
+    # Expose the function to the command-line
+    fire.Fire(upload_lfs_files)
+
+
+if __name__ == "__main__":
+    main()
--- tali_wit/scripts/upload_tali_data_to_hub.py
+++ tali_wit/scripts/upload_tali_data_to_hub.py
@@ -1,16 +1,42 @@
 import os
-
+import glob
+import pathlib
 import huggingface_hub as hf_hub
+from tqdm import tqdm
 
+os.environ["HF_TOKEN"] = "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"
+
+dataset_dir = "/data-unified/TALI"
 client = hf_hub.HfApi(token=os.environ["HF_TOKEN"])
 repo = hf_hub.Repository(
-    local_dir="/data-fast1/datasets/",
+    local_dir=dataset_dir,
     clone_from="Antreas/TALI",
     token=client.token,
     repo_type="dataset",
+    client=client,
 )
 repo.git_pull()
 
-repo.git_add("data/**", auto_lfs_track=True)
-repo.git_commit("Update TALI dataset")
-repo.git_push()
+# Find files with "video_data_part" in their name
+files = glob.glob(f"{dataset_dir}/**")
+
+# Iterate through the files with tqdm progress bar, adding and committing each one
+count = 1
+upload_processes = []
+for file in tqdm(files):
+    print(f"Uploading {os.path.basename(file)}")
+    try:
+        repo.git_add(file, auto_lfs_track=True)
+        repo.git_commit(f"Upload {os.path.basename(file)}")
+        count += 1
+    except Exception as e:
+        print(f"Error: {e}")
+
+    if count % 1 == 0:
+        push_process = repo.git_push(blocking=True)
+        upload_processes.append(push_process)
+
+# while len(upload_processes) > 0:
+#     for process in upload_processes:
+#         if process.is_done is True:
+#             upload_processes.remove(process)
