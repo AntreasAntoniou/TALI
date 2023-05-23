c04143f824ddca754bfaf0f30d6b3e3645a33efc
Antreas Antoniou <iam@antreas.io>
2023-05-23 03:25:56 +0100

    updated

--- tali/scripts/download_tali.py
+++ tali/scripts/download_tali.py
@@ -1,11 +1,20 @@
 import multiprocessing as mp
-from huggingface_hub import snapshot_download
+from huggingface_hub import snapshot_download, hf_hub_download
 
-snapshot_download(
+# snapshot_download(
+#     repo_id="Antreas/TALI",
+#     repo_type="dataset",
+#     cache_dir="/tali-data/TALI",
+#     local_dir="/tali-data/TALI",
+#     resume_download=True,
+#     max_workers=mp.cpu_count(),
+# )
+
+hf_hub_download(
+    filename="caption_data.7z",
+    folder_path="data/",
     repo_id="Antreas/TALI",
     repo_type="dataset",
     cache_dir="/tali-data/TALI",
     local_dir="/tali-data/TALI",
-    resume_download=True,
-    max_workers=mp.cpu_count(),
 )
--- tali/scripts/upload_with_hug.py
+++ tali/scripts/upload_with_hug.py
@@ -1,30 +1,33 @@
 import os
 from tqdm.auto import tqdm
 
-# os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
+os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "0"
 
 from huggingface_hub import HfApi
 
-target_dir = "/data-fast1/TALI/data/"
+# target_dir = "/data-fast1/TALI/data/"
+print(f"Uploading stuff to Antreas/TALI")
 api = HfApi()
 # video_data_part.7z.001"
 
 
-for subdir, dirs, files in tqdm(os.walk(target_dir)):
-    if "subdir" not in subdir:
-        continue
-    print(f"Uploading {subdir}, to, data/{subdir.split('/')[-1]}")
-    api.upload_folder(
-        folder_path=subdir,
-        path_in_repo=f"data/{subdir.split('/')[-1]}",
-        repo_id="Antreas/TALI",
-        repo_type="dataset",
-    )
+# for subdir, dirs, files in tqdm(os.walk(target_dir)):
+#     if "subdir" not in subdir:
+#         continue
+#     print(f"Uploading {subdir}, to, data/{subdir.split('/')[-1]}")
+#     api.upload_folder(
+#         folder_path=subdir,
+#         path_in_repo=f"data/{subdir.split('/')[-1]}",
+#         repo_id="Antreas/TALI",
+#         repo_type="dataset",
+#     )
 
 # target_dir = "/data-fast1/TALI/data/subdir_7"
-# api.upload_folder(
-#     folder_path=target_dir,
-#     path_in_repo=f"data/{target_dir.split('/')[-1]}",
-#     repo_id="Antreas/TALI",
-#     repo_type="dataset",
-# )
+result = api.upload_file(
+    path_or_fileobj="/data-fast1/TALI/caption_data.7z",
+    path_in_repo="data/caption_data.7z",
+    repo_id="Antreas/TALI",
+    repo_type="dataset",
+    token=os.environ["HF_TOKEN"],
+)
+print(result)
