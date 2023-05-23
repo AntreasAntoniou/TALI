a3ac9b5c47ddd1498a7822fd2eda80226de12b71
Antreas Antoniou <iam@antreas.io>
2023-05-23 04:43:30 +0100

    update

--- tali/scripts/validate_dataset.py
+++ tali/scripts/validate_dataset.py
@@ -24,7 +24,8 @@ if __name__ == "__main__":
                 )
                 if video_path.exists():
                     updated_video_list.append(video_path.as_posix())
-
+            if len(updated_video_list) == 0:
+                continue
             item["youtube_content_video"] = updated_video_list
             yield item
 
@@ -37,6 +38,7 @@ if __name__ == "__main__":
         updated_video_list = []
         for item in tqdm(dataset):
             video_list = item["youtube_content_video"]
+
             for video_path in video_list:
                 video_path: pathlib.Path = (
                     pathlib.Path(
@@ -46,7 +48,8 @@ if __name__ == "__main__":
                 )
                 if video_path.exists():
                     updated_video_list.append(video_path.as_posix())
-
+            if len(updated_video_list) == 0:
+                continue
             item["youtube_content_video"] = updated_video_list
             yield item
 
@@ -77,8 +80,8 @@ if __name__ == "__main__":
             item["youtube_content_video"] = updated_video_list
             yield item
 
-    # train_data = datasets.Dataset.from_generator(train_generator)
-    # train_data.save_to_disk(pathlib.Path(tali_dataset_dir) / f"train-set")
+    train_data = datasets.Dataset.from_generator(train_generator, num_proc=64)
+    train_data.save_to_disk(pathlib.Path(tali_dataset_dir) / f"train-set")
     val_data = datasets.Dataset.from_generator(
         val_generator, writer_batch_size=1000
     )
