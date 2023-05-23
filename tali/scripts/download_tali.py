6a82039593f3e1906310a217f4896a216eaa95ad
Antreas Antoniou <iam@antreas.io>
2023-05-23 05:04:46 +0100

    update

--- tali/scripts/download_tali.py
+++ tali/scripts/download_tali.py
@@ -1,20 +1,20 @@
 import multiprocessing as mp
 from huggingface_hub import snapshot_download, hf_hub_download
 
-# snapshot_download(
-#     repo_id="Antreas/TALI",
-#     repo_type="dataset",
-#     cache_dir="/tali-data/TALI",
-#     local_dir="/tali-data/TALI",
-#     resume_download=True,
-#     max_workers=mp.cpu_count(),
-# )
-
-hf_hub_download(
-    filename="caption_data.7z",
-    subfolder="data",
+snapshot_download(
     repo_id="Antreas/TALI",
     repo_type="dataset",
     cache_dir="/tali-data/TALI",
     local_dir="/tali-data/TALI",
+    resume_download=True,
+    max_workers=mp.cpu_count(),
 )
+
+# hf_hub_download(
+#     filename="caption_data.7z",
+#     subfolder="data",
+#     repo_id="Antreas/TALI",
+#     repo_type="dataset",
+#     cache_dir="/tali-data/TALI",
+#     local_dir="/tali-data/TALI",
+# )
