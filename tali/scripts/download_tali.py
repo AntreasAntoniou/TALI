8d2283fbaeea5db8766c06c370a6d340310fa11e
Antreas Antoniou <iam@antreas.io>
2023-05-23 05:15:41 +0100

    update

--- tali/scripts/download_tali.py
+++ tali/scripts/download_tali.py
@@ -5,7 +5,7 @@ snapshot_download(
     repo_id="Antreas/TALI",
     repo_type="dataset",
     cache_dir="/tali-data/TALI",
-    local_dir="/tali-data/TALI",
+    local_dir="/tali-data/",
     resume_download=True,
     max_workers=mp.cpu_count(),
 )
