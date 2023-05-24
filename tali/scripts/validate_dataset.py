17220d53096586ed8449d61decacf1774acf14a2
Antreas Antoniou <iam@antreas.io>
2023-05-24 01:31:16 +0100

    update

--- tali/scripts/validate_dataset.py
+++ tali/scripts/validate_dataset.py
@@ -5,121 +5,93 @@ from tqdm.auto import tqdm
 tali_dataset_dir = "/tali-data/"
 
 if __name__ == "__main__":
-    # def train_generator():
-    #     set_name = "train"
-    #     dataset = datasets.load_from_disk(
-    #         pathlib.Path(tali_dataset_dir) / f"{set_name}-set",
-    #         keep_in_memory=True,
-    #     )
-    #     updated_video_list = []
-    #     for item in dataset:
-    #         video_list = item["youtube_content_video"]
-    #         for video_path in video_list:
-    #             video_path: pathlib.Path = (
-    #                 pathlib.Path(
-    #                     tali_dataset_dir.replace("/data/", tali_dataset_dir)
-    #                 )
-    #                 / video_path
-    #             )
-    #             print(
-    #                 f"Video {video_path.as_posix()}, exists: {video_path.exists()}"
-    #             )
-    #             if video_path.exists():
-    #                 updated_video_list.append(video_path.as_posix())
-    #         if len(updated_video_list) == 0:
-    #             continue
-    #         item["youtube_content_video"] = updated_video_list
-    #         print(list(item.keys()))
-    #         yield item
 
-    # def val_generator():
-    #     set_name = "val"
-    #     dataset = datasets.load_from_disk(
-    #         pathlib.Path(tali_dataset_dir) / f"{set_name}-set",
-    #         keep_in_memory=True,
-    #     )
-    #     updated_video_list = []
-    #     for item in dataset:
-    #         video_list = item["youtube_content_video"]
+    def train_generator():
+        set_name = "train"
+        dataset = datasets.load_from_disk(
+            pathlib.Path(tali_dataset_dir) / f"{set_name}-set",
+            keep_in_memory=True,
+        )
 
-    #         for video_path in video_list:
-    #             video_path: pathlib.Path = (
-    #                 pathlib.Path(
-    #                     tali_dataset_dir.replace("/data/", tali_dataset_dir)
-    #                 )
-    #                 / video_path
-    #             )
-    #             if video_path.exists():
-    #                 updated_video_list.append(video_path.as_posix())
-    #         if len(updated_video_list) == 0:
-    #             continue
-    #         item["youtube_content_video"] = updated_video_list
-    #         yield item
+        for item in tqdm(dataset):
+            video_list = item["youtube_content_video"]
+            updated_video_list = []
+            for video_path in video_list:
+                temp_path = video_path.replace(
+                    "/data/", tali_dataset_dir + "TALI/"
+                )
+                video_path_actual: pathlib.Path = pathlib.Path(temp_path)
 
-    # def test_generator():
-    #     set_name = "test"
-    #     dataset = datasets.load_from_disk(
-    #         pathlib.Path(tali_dataset_dir) / f"{set_name}-set",
-    #         keep_in_memory=True,
-    #     )
-    #     updated_video_list = []
-    #     for item in dataset:
-    #         video_list = item["youtube_content_video"]
-    #         for video_path in video_list:
-    #             video_path: pathlib.Path = (
-    #                 pathlib.Path(
-    #                     tali_dataset_dir.replace("/data/", tali_dataset_dir)
-    #                 )
-    #                 / video_path
-    #             )
-    #             if video_path.exists():
-    #                 updated_video_list.append(
-    #                     video_path.as_posix().replace(
-    #                         tali_dataset_dir, "/data/"
-    #                     )
-    #                 )
-    #         if len(updated_video_list) == 0:
-    #             continue
-    #         item["youtube_content_video"] = updated_video_list
-    #         yield item
+                if video_path_actual.exists():
+                    updated_video_list.append(video_path)
 
-    # for item in train_generator():
-    #     print(item)
+            if len(updated_video_list) == 0:
+                continue
 
-    # train_data = datasets.Dataset.from_generator(train_generator, num_proc=64)
-    # train_data.save_to_disk(pathlib.Path(tali_dataset_dir) / f"train-set")
-    # val_data = datasets.Dataset.from_generator(
-    #     val_generator, writer_batch_size=1000
-    # )
-    # val_data.save_to_disk(pathlib.Path(tali_dataset_dir) / f"val-set")
-    # test_data = datasets.Dataset.from_generator(
-    #     test_generator, writer_batch_size=1000
-    # )
-    # test_data.save_to_disk(pathlib.Path(tali_dataset_dir) / f"test-set")
+            item["youtube_content_video"] = updated_video_list
+            yield item
 
-    set_name = "train"
-    dataset = datasets.load_from_disk(
-        pathlib.Path(tali_dataset_dir) / f"{set_name}-set",
-        keep_in_memory=True,
+    def val_generator():
+        set_name = "val"
+        dataset = datasets.load_from_disk(
+            pathlib.Path(tali_dataset_dir) / f"{set_name}-set",
+            keep_in_memory=True,
+        )
+
+        for item in tqdm(dataset):
+            video_list = item["youtube_content_video"]
+            updated_video_list = []
+            for video_path in video_list:
+                temp_path = video_path.replace(
+                    "/data/", tali_dataset_dir + "TALI/"
+                )
+                video_path_actual: pathlib.Path = pathlib.Path(temp_path)
+
+                if video_path_actual.exists():
+                    updated_video_list.append(video_path)
+
+            if len(updated_video_list) == 0:
+                continue
+
+            item["youtube_content_video"] = updated_video_list
+            yield item
+
+    def test_generator():
+        set_name = "test"
+        dataset = datasets.load_from_disk(
+            pathlib.Path(tali_dataset_dir) / f"{set_name}-set",
+            keep_in_memory=True,
+        )
+
+        for item in tqdm(dataset):
+            video_list = item["youtube_content_video"]
+            updated_video_list = []
+            for video_path in video_list:
+                temp_path = video_path.replace(
+                    "/data/", tali_dataset_dir + "TALI/"
+                )
+                video_path_actual: pathlib.Path = pathlib.Path(temp_path)
+
+                if video_path_actual.exists():
+                    updated_video_list.append(video_path)
+
+            if len(updated_video_list) == 0:
+                continue
+
+            item["youtube_content_video"] = updated_video_list
+            yield item
+
+    train_data = datasets.Dataset.from_generator(train_generator, num_proc=64)
+    train_data.save_to_disk(pathlib.Path(tali_dataset_dir) / f"train-set")
+    val_data = datasets.Dataset.from_generator(
+        val_generator, writer_batch_size=1000
     )
+    val_data.save_to_disk(pathlib.Path(tali_dataset_dir) / f"val-set")
+    test_data = datasets.Dataset.from_generator(
+        test_generator, writer_batch_size=1000
+    )
+    test_data.save_to_disk(pathlib.Path(tali_dataset_dir) / f"test-set")
 
-    for item in dataset:
-        video_list = item["youtube_content_video"]
-        updated_video_list = []
-        for video_path in video_list:
-            temp_path = video_path.replace(
-                "/data/", tali_dataset_dir + "TALI/"
-            )
-            video_path_actual: pathlib.Path = pathlib.Path(temp_path)
-            print(
-                f"Video {video_path_actual.as_posix()}, exists: {video_path_actual.exists()}"
-            )
-            if video_path_actual.exists():
-                updated_video_list.append(video_path)
-
-        if len(updated_video_list) == 0:
-            continue
-
-        item["youtube_content_video"] = updated_video_list
-        print(list(item.keys()))
-        print(len(item["youtube_content_video"]))
+    train_data.push_to_hub(repo_id="Antreas/TALI", repo_type="dataset")
+    val_data.push_to_hub(repo_id="Antreas/TALI", repo_type="dataset")
+    test_data.push_to_hub(repo_id="Antreas/TALI", repo_type="dataset")
