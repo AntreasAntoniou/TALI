e0ce8479223fba5f8c8bd714c9a29590606972db
Antreas Antoniou <iam@antreas.io>
2023-05-25 04:03:25 +0000

    updates

--- tali/scripts/download_tali.py
+++ tali/scripts/download_tali.py
@@ -1,20 +1,81 @@
 import multiprocessing as mp
-from huggingface_hub import snapshot_download, hf_hub_download
+from huggingface_hub import snapshot_download
 
-snapshot_download(
+# snapshot_download(
+#     repo_id="Antreas/TALI",
+#     repo_type="dataset",
+#     cache_dir="/tali-data/",
+#     local_dir="/tali-data/",
+#     resume_download=True,
+#     max_workers=mp.cpu_count(),
+# )
+
+from huggingface_hub import hf_hub_download
+
+hf_hub_download(
     repo_id="Antreas/TALI",
-    repo_type="dataset",
-    cache_dir="/tali-data/TALI",
-    local_dir="/tali-data/",
-    resume_download=True,
-    max_workers=mp.cpu_count(),
+    subdir="train-set",
+    filename="data-00000-of-00001.arrow",
 )
 
 hf_hub_download(
-    filename="caption_data.7z",
-    subfolder="data",
     repo_id="Antreas/TALI",
-    repo_type="dataset",
-    cache_dir="/tali-data/TALI",
-    local_dir="/tali-data/TALI",
+    subdir="train-set",
+    filename="dataset_info.json",
+    cache_dir="/tali-data/",
+    local_dir="/tali-data/",
+)
+
+hf_hub_download(
+    repo_id="Antreas/TALI",
+    subdir="train-set",
+    filename="state.json",
+    cache_dir="/tali-data/",
+    local_dir="/tali-data/",
+)
+####################
+hf_hub_download(
+    repo_id="Antreas/TALI",
+    subdir="val-set",
+    filename="data-00000-of-00001.arrow",
+)
+
+hf_hub_download(
+    repo_id="Antreas/TALI",
+    subdir="val-set",
+    filename="dataset_info.json",
+    cache_dir="/tali-data/",
+    local_dir="/tali-data/",
+)
+
+hf_hub_download(
+    repo_id="Antreas/TALI",
+    subdir="val-set",
+    filename="state.json",
+    cache_dir="/tali-data/",
+    local_dir="/tali-data/",
+)
+####################
+
+for i in range(9):
+    hf_hub_download(
+        repo_id="Antreas/TALI",
+        subdir="train-set",
+        filename=f"data-0000{i}-of-00009.arrow",
+    )
+
+hf_hub_download(
+    repo_id="Antreas/TALI",
+    subdir="train-set",
+    filename="dataset_info.json",
+    cache_dir="/tali-data/",
+    local_dir="/tali-data/",
+)
+
+hf_hub_download(
+    repo_id="Antreas/TALI",
+    subdir="train-set",
+    filename="state.json",
+    cache_dir="/tali-data/",
+    local_dir="/tali-data/",
 )
