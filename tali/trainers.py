4c39de0457a2ccae9f2801da77215448eef3e30e
Antreas Antoniou <iam@antreas.io>
2023-05-26 06:47:46 +0000

    updates

--- tali/trainers.py
+++ tali/trainers.py
@@ -1,6 +1,7 @@
 import time
 from dataclasses import dataclass
 from typing import Any, Dict
+import accelerate
 
 import torch
 from accelerate import Accelerator
@@ -55,6 +56,15 @@ class ClassificationTrainer(Trainer):
         scheduler_interval: str = Interval.STEP,
         experiment_tracker: Any = None,
     ):
+        """
+        Initializes the ClassificationTrainer class.
+
+        Args:
+            optimizer: The optimizer to be used during training.
+            scheduler: The learning rate scheduler, defaults to None.
+            scheduler_interval: Interval to update the learning rate, either "step" or "epoch".
+            experiment_tracker: A tracker to log experimental details, defaults to None.
+        """
         super().__init__()
 
         self.optimizer = optimizer
@@ -66,46 +76,60 @@ class ClassificationTrainer(Trainer):
             assert scheduler_interval in {"step", "epoch"}
             self.scheduler_interval = scheduler_interval
 
-    def get_optimizer(self):
-        return self.optimizer
+    @staticmethod
+    def _calculate_metric_mean(
+        output_dict: Dict[str, torch.Tensor], metric: str
+    ) -> torch.Tensor:
+        """
+        Helper function to calculate the mean of a specific metric from the output dictionary.
+
+        Args:
+            output_dict: A dictionary containing output metrics.
+            metric: The specific metric to calculate the mean for.
+
+        Returns:
+            The mean value of the specific metric.
+        """
+        return torch.mean(
+            torch.stack(
+                [
+                    value.cpu()
+                    for key, value in output_dict.items()
+                    if metric in key
+                ]
+            )
+        )
 
     def step(self, model, batch, global_step, accelerator: Accelerator):
+        """
+        Performs a single step in the training process.
+
+        Args:
+            model: The model to be trained.
+            batch: The batch of data for this step.
+            global_step: The current global step in the training process.
+            accelerator: The PyTorch accelerator.
+
+        Returns:
+            A StepOutput object containing the output_dict, loss, accuracy, and accuracy_top_5.
+        """
         try:
             output_dict = model.forward(batch, return_loss=True)
-            loss = torch.mean(
-                torch.stack(
-                    [
-                        value
-                        for key, value in output_dict.items()
-                        if "_loss" in key
-                    ]
-                )
+            loss = self._calculate_metric_mean(output_dict, "_loss")
+            accuracy = self._calculate_metric_mean(output_dict, "_accuracy")
+            accuracy_top_5 = self._calculate_metric_mean(
+                output_dict, "_accuracy_top_5"
             )
-            accuracy = torch.mean(
-                torch.stack(
-                    [
-                        value.cpu()
-                        for key, value in output_dict.items()
-                        if "_accuracy" in key
-                    ]
-                )
-            )
-            accuracy_top_5 = torch.mean(
-                torch.stack(
-                    [
-                        value.cpu()
-                        for key, value in output_dict.items()
-                        if "_accuracy_top_5" in key
-                    ]
-                )
-            )
-            keys = list(output_dict.keys())
 
+            keys = list(output_dict.keys())
             for key in keys:
                 if "_loss" not in key and "_accuracy" not in key:
                     del output_dict[key]
 
             accelerator.backward(loss)
+
+            if accelerator.sync_gradients:
+                accelerator.clip_grad_norm_(model.parameters(), max_norm=1.0)
 
             return StepOutput(
                 output_dict=output_dict,
@@ -115,27 +139,33 @@ class ClassificationTrainer(Trainer):
             )
         except Exception as e:
             logger.warning(e)
-            # some_key = list(batch.keys())
-            # some_sub_key = list(batch[some_key[0]].keys())
-            # some_value = batch[some_key[0]][some_sub_key[0]]
-            # if some_value.shape[0] > 1:
-            #     smaller_batch_dict = {}
-
-            #     for key, value in batch.items():
-            #         smaller_batch_dict[key] = {}
-            #         logger.info(f"{key}")
-            #         for sub_key, sub_value in value.items():
-            #             logger.info(
-            #                 f"sub_key, sub_value.shape: {sub_key}, {sub_value.shape}"
-            #             )
-            #             smaller_batch_dict[key][sub_key] = sub_value[1:]
-            #             logger.info(
-            #                 f"smaller_batch_dict[key][sub_key].shape: {smaller_batch_dict[key][sub_key].shape}"
-            #             )
-
-            #     return self.step(model, smaller_batch_dict, global_step, accelerator)
-            # else:
             return None
+
+    @staticmethod
+    def _generate_sample_from_batch(
+        batch: Dict,
+        modality_a: str,
+        sub_modality_a: str,
+        modality_b: str,
+        sub_modality_b: str,
+    ) -> Dict:
+        """
+        Helper function to generate a sample from a batch.
+
+        Args:
+            batch: The batch of data.
+            modality_a: The first modality in the pair.
+            sub_modality_a: The sub-modality related to the first modality.
+            modality_b: The second modality in the pair.
+            sub_modality_b: The sub-modality related to the second modality.
+
+        Returns:
+            A sample generated from the batch.
+        """
+        return {
+            modality_a: {sub_modality_a: batch[modality_a][sub_modality_a]},
+            modality_b: {sub_modality_b: batch[modality_b][sub_modality_b]},
+        }
 
     @collect_metrics
     def training_step(
@@ -145,67 +175,67 @@ class ClassificationTrainer(Trainer):
         global_step,
         accelerator: Accelerator,
     ) -> TrainerOutput:
+        """
+        Performs a training step.
+
+        Args:
+            model: The model to be trained.
+            batch: The batch of data for this step.
+            global_step: The current global step in the training process.
+            accelerator: The PyTorch accelerator.
+
+        Returns:
+            A TrainerOutput object containing the metrics and other details of the training step.
+        """
         model.train()
+        with accelerate.accumulate(model):
+            overall_loss = []
+            overall_accuracy = []
+            overall_accuracy_top_5 = []
+            overall_output_dict = {}
+            batch = generate_hierarchical_data_dict(batch)
 
-        overall_loss = []
-        overall_accuracy = []
-        overall_accuracy_top_5 = []
-        overall_output_dict = {}
-        generate_hierarchical_batch_start_time = time.time()
-        batch = generate_hierarchical_data_dict(batch)
-        generate_hierarchical_batch_end_time = time.time()
-        logger.debug(
-            f"generate_hierarchical_batch_time: {generate_hierarchical_batch_end_time - generate_hierarchical_batch_start_time}"
-        )
+            possible_pairs = extract_all_possible_pairs(batch)
+            self.optimizer.zero_grad()
 
-        possible_pairs_start_time = time.time()
-        possible_pairs = extract_all_possible_pairs(batch)
-        possible_pairs_end_time = time.time()
-        logger.debug(
-            f"possible_pairs_time: {possible_pairs_end_time - possible_pairs_start_time}"
-        )
-        self.optimizer.zero_grad()
+            for (
+                modality_a,
+                sub_modality_a,
+                modality_b,
+                sub_modality_b,
+            ) in possible_pairs:
+                sample = self._generate_sample_from_batch(
+                    batch,
+                    modality_a,
+                    sub_modality_a,
+                    modality_b,
+                    sub_modality_b,
+                )
 
-        for (
-            modality_a,
-            sub_modality_a,
-            modality_b,
-            sub_modality_b,
-        ) in possible_pairs:
-            sample = {
-                modality_a: {
-                    sub_modality_a: batch[modality_a][sub_modality_a]
-                },
-                modality_b: {
-                    sub_modality_b: batch[modality_b][sub_modality_b]
-                },
-            }
+                step_output: StepOutput = self.step(
+                    model=model,
+                    batch=sample,
+                    global_step=global_step,
+                    accelerator=accelerator,
+                )
+                if step_output is not None:
+                    overall_output_dict |= step_output.output_dict
+                    overall_loss.append(step_output.loss)
+                    overall_accuracy.append(step_output.accuracy)
+                    overall_accuracy_top_5.append(step_output.accuracy_top_5)
 
-            step_output: StepOutput = self.step(
-                model=model,
-                batch=sample,
-                global_step=global_step,
-                accelerator=accelerator,
-            )
-            if step_output is not None:
-                overall_output_dict |= step_output.output_dict
-                overall_loss.append(step_output.loss)
-                overall_accuracy.append(step_output.accuracy)
-                overall_accuracy_top_5.append(step_output.accuracy_top_5)
+            self.optimizer.step()
 
-        self.optimizer.step()
-
-        if len(overall_loss) > 0:
+        metrics = {}
+        if overall_loss:
             metrics = {
                 "accuracy": torch.mean(torch.stack(overall_accuracy)),
                 "accuracy_top_5": torch.mean(
                     torch.stack(overall_accuracy_top_5)
                 ),
                 "loss": torch.mean(torch.stack(overall_loss)),
+                **overall_output_dict,
             }
-            metrics |= overall_output_dict
-        else:
-            metrics = {}
 
         for key, value in metrics.items():
             self.state_dict.setdefault(key, []).append(value)
@@ -215,41 +245,9 @@ class ClassificationTrainer(Trainer):
         return TrainerOutput(
             phase_name="training",
             opt_loss=torch.mean(torch.stack(overall_loss))
-            if len(overall_loss) > 0
+            if overall_loss
             else None,
-            global_step=global_step,
             metrics=metrics,
-            experiment_tracker=self.experiment_tracker,
-        )
-
-    @collect_metrics
-    def start_training(
-        self,
-        global_step: int,
-    ):
-        self.state_dict = {}
-        return TrainerOutput(
-            opt_loss=None,
-            global_step=global_step,
-            metrics={},
-            phase_name="training",
-            experiment_tracker=self.experiment_tracker,
-        )
-
-    @collect_metrics
-    def end_training(
-        self,
-        global_step: int,
-    ):
-        epoch_metrics = {}
-        for key, value in self.state_dict.items():
-            epoch_metrics[f"{key}-epoch-mean"] = torch.stack(value).mean()
-            epoch_metrics[f"{key}-epoch-std"] = torch.stack(value).std()
-
-        return TrainerOutput(
-            opt_loss=None,
-            global_step=global_step,
-            metrics=epoch_metrics,
             phase_name="training",
             experiment_tracker=self.experiment_tracker,
         )
