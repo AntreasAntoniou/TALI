49595a6bd9badbbc5d5af64b19d7c555a5e039bc
Antreas Antoniou <iam@antreas.io>
2023-06-08 20:57:35 +0100

    updates

--- tali/boilerplate.py
+++ tali/boilerplate.py
@@ -542,12 +542,12 @@ class Learner(nn.Module):
             state_dict["eval"],
         )
 
-        if (checkpoint_path / "optimizer.bin").exists():
-            (checkpoint_path / "optimizer.bin").unlink()
+        # if (checkpoint_path / "optimizer.bin").exists():
+        #     (checkpoint_path / "optimizer.bin").unlink()
 
         self.accelerator.load_state(checkpoint_path)
 
-        self.trainer.optimizer = self.accelerator.prepare(self.trainer.optimizer)
+        # self.trainer.optimizer = self.accelerator.prepare(self.trainer.optimizer)
 
         self.callback_handler.on_load_checkpoint(
             model=self.model,
