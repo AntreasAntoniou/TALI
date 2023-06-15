1ee1ddeb2bbe5f62fce6158eeb2c66a079ecdefa
Antreas Antoniou <iam@antreas.io>
2023-06-15 02:45:35 +0100

    updates

--- tali/config.py
+++ tali/config.py
@@ -203,7 +203,7 @@ class BaseConfig:
 # future args, and makes it a lot easier to add whatever we need from the command line
 batch_size_multiplier = 2
 default_video_batch_size = 16
-default_audio_batch_size = 16
+default_audio_batch_size = 32
 default_image_text_batch_size = 240
 
 
