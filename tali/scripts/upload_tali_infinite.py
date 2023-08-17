0c30c9970d0a8db7562de3f4eecaabf6a8310daa
Antreas Antoniou <iam@antreas.io>
2023-08-17 18:43:28 +0100

    updates

--- tali/scripts/upload_tali_infinite.py
+++ tali/scripts/upload_tali_infinite.py
@@ -1,6 +1,7 @@
+import signal
 import subprocess
 import time
-import signal
+
 
 def run_command():
     command = [
@@ -22,15 +23,17 @@ def run_command():
         while True:
             time_elapsed = time.time() - start_time
 
-            # If 15 minutes have passed, terminate the process
-            if time_elapsed >= 15 * 60:
+            # If 60 minutes have passed, terminate the process
+            if time_elapsed >= 60 * 60:
                 process.send_signal(signal.SIGTERM)
                 break
 
             # Check if the process has terminated (due to error or completion)
             return_code = process.poll()
             if return_code is not None:
-                print(f"Process exited with return code {return_code}. Restarting...")
+                print(
+                    f"Process exited with return code {return_code}. Restarting..."
+                )
                 break
 
             # Sleep for a short duration to avoid busy-waiting
@@ -38,6 +41,7 @@ def run_command():
 
         # Wait for the process to terminate before restarting
         process.wait()
+
 
 if __name__ == "__main__":
     run_command()
